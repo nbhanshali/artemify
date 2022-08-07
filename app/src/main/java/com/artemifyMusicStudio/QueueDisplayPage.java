b1f0229b41f063798f6727913c3a87d06c9821fd
nbhanshali <nbhanshali@gmail.com>
2022-08-07 15:15:37 -0400

    Fixed exit button on QueueDisplayPage

--- app/src/main/java/com/artemifyMusicStudio/QueueDisplayPage.java
+++ app/src/main/java/com/artemifyMusicStudio/QueueDisplayPage.java
@@ -6,6 +6,7 @@ import com.artemifyMusicStudio.controller.CommandItemType;
 import com.artemifyMusicStudio.controller.SimpleButtonCommandCreator;
 import com.artemifyMusicStudio.controller.commandCreator.PopupCommandCreator;
 import com.artemifyMusicStudio.controller.commandCreator.QueueServiceCommandCreator;
+import com.artemifyMusicStudio.controller.commandCreator.TransitionCommandCreator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +36,8 @@ public class QueueDisplayPage extends PageActivity{
                 return new QueueServiceCommandCreator(this.activityServiceCache);
             case "PopupCommandCreator" :
                 return new PopupCommandCreator(this.activityServiceCache);
+            case "TransitionCommandCreator" :
+                return new TransitionCommandCreator(this.activityServiceCache);
             default:
                 return null;
         }
@@ -69,14 +72,17 @@ public class QueueDisplayPage extends PageActivity{
                         CommandItemType.SKIP_SONG,
                         CommandItemType.PLAY_PREVIOUS_SONG,
                         CommandItemType.REPEAT_SONG_ONCE,
-                        CommandItemType.REPEAT_SONG_INF,
-                        CommandItemType.EXIT_PAGE)
+                        CommandItemType.REPEAT_SONG_INF)
         );
         ArrayList<CommandItemType> tempList2 = new ArrayList<>(
                 List.of(CommandItemType.POP_UP_REMOVE_FROM_QUEUE_DIALOG)
         );
+        ArrayList<CommandItemType> tempList3 = new ArrayList<>(
+                List.of(CommandItemType.EXIT_PAGE)
+        );
         menuCommandCreatorMap.put("QueueServiceCommandCreator", tempList1);
         menuCommandCreatorMap.put("PopupCommandCreator", tempList2);
+        menuCommandCreatorMap.put("TransitionCommandCreator", tempList3);
     }
 
     @Override
